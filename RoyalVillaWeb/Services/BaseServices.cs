cefeb3441aae55d0cefa56cb34072352a0c389d4
Vaishnavi <vaishnavitonpe0@gmail.com>
2026-02-20 17:33:50 +0530

    feat: Update BaseServices to include token authorization and improve error handling; clean up Index view and adjust VillaAPI URL in appsettings

--- RoyalVillaWeb/Services/BaseServices.cs
+++ RoyalVillaWeb/Services/BaseServices.cs
@@ -25,34 +25,30 @@ namespace RoyalVillaWeb.Services
         }
        public async Task<T?> SendAsync<T>(ApiRequest apiRequest)
         {
-            try
+            var client = _httpClient.CreateClient("RoyalVillaAPI");
+            if (string.IsNullOrEmpty(apiRequest.Url))
+                throw new ArgumentNullException(nameof(apiRequest.Url), "API request URL cannot be null or empty.");
+
+            var message = new HttpRequestMessage
             {
-                var client = _httpClient.CreateClient("RoyalVillaAPI");
-                if (string.IsNullOrEmpty(apiRequest.Url))
-                    throw new ArgumentNullException(nameof(apiRequest.Url), "API request URL cannot be null or empty.");
-                var message = new HttpRequestMessage
-                {
-                    RequestUri = new Uri(apiRequest.Url, uriKind: UriKind.Relative),
-                    Method = GetHttpMethod(apiRequest.ApiType),
-                };
+                RequestUri = new Uri(apiRequest.Url, uriKind: UriKind.Relative),
+                Method = GetHttpMethod(apiRequest.ApiType),
+            };
 
-                if(apiRequest.Data != null)
-                {
-                   message.Content = JsonContent.Create(apiRequest.Data, options: JsonOptions); 
-                }
-
-                var apiResponse = await client.SendAsync(message);
-
-                return await apiResponse.Content.ReadFromJsonAsync<T>(JsonOptions);
-
-            }
-            catch (System.Exception ex)
+            if (apiRequest.Data != null)
             {
-                
-                Console.WriteLine($"Unexpected error : {ex.Message}");
-                return default;
-
+                message.Content = JsonContent.Create(apiRequest.Data, options: JsonOptions);
             }
+
+            if (!string.IsNullOrEmpty(apiRequest.Token))
+            {
+                message.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", apiRequest.Token);
+            }
+
+            var apiResponse = await client.SendAsync(message);
+            apiResponse.EnsureSuccessStatusCode();
+
+            return await apiResponse.Content.ReadFromJsonAsync<T>(JsonOptions);
         }
 
         private static HttpMethod GetHttpMethod(StaticDetails.ApiType apiType)
